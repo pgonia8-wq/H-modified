eb8cd626521821b2b8db22c6d9a631cab4983ace
pgonia8-wq <pgonia8@gmail.com>
2026-03-05 13:10:23 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider';
+import { ThemeProvider } from './lib/ThemeContext'
+import HomePage from './pages/HomePage.tsx'
+import ChatPage from './pages/chat/ChatPage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MiniKitProvider>
-      <App />
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </MiniKitProvider>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
