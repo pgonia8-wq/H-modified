6709b1e5552b9993d733287072b9ec2fb8e819cc
pgonia8-wq <pgonia8@gmail.com>
2026-03-06 17:05:56 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { ThemeProvider } from './lib/ThemeContext'
+import App from './App.tsx'
 import ChatPage from './pages/chat/ChatPage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
