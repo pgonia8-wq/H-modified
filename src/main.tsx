2aa5f40cc5895bae99581d29192c6f80872cebfb
pgonia8-wq <pgonia8@gmail.com>
2026-03-13 23:59:09 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,13 @@ import "./index.css";
 import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider";
 import { ThemeProvider } from "./lib/ThemeContext";
 import { UserProvider } from "./context/UserContext";
-import { LanguageProvider } from "./LanguageContext"; // <-- importamos
+import { LanguageProvider } from "./LanguageContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MiniKitProvider appId="app_6a98c88249208506dcd4e04b529111fc">
     <UserProvider>
       <ThemeProvider>
-        <LanguageProvider> {/* <-- envolvemos App */}
+        <LanguageProvider>
           <App />
         </LanguageProvider>
       </ThemeProvider>
